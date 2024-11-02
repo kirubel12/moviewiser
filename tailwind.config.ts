40c121427162bb6a6b6ac66f2afbab90e4f99c31
kirubel12 <kirubelhabte1991@gmail.com>
2024-11-02 12:17:41 +0300

    fix:cleanup code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
   ],
   theme: {
   	extend: {
+        fontFamily: {
+            'poppins': 'Poppins'
+        },
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
